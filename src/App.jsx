7e311e6cfec7e1502f630204c011532323d00623
PushDream <payneboat@gmail.com>
2026-07-18 14:52:25 -0700

    Revert "feat: add interactive route map with live bus tracking"
    This reverts commit fc94c5989ff23656d52e1ccd1f9a69814ef17bde.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import RoutesSection from "./components/RoutesSection.jsx";
 import TicketSystem from "./components/TicketSystem.jsx";
 import StopsSection from "./components/StopsSection.jsx";
 import ServiceBand from "./components/ServiceBand.jsx";
-import RouteMapSection from "./components/RouteMapSection.jsx";
 import ManageBooking from "./components/ManageBooking.jsx";
 import FAQSection from "./components/FAQSection.jsx";
 import ReviewsContact from "./components/ReviewsContact.jsx";
@@ -132,8 +131,6 @@ export default function App() {
       <StopsSection t={t} />
 
       <ServiceBand t={t} />
-
-      <RouteMapSection activeFare={activeFare} dark={dark} t={t} />
 
       <ManageBooking t={t} />
 
